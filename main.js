ddf32b072976f98672a323896e5d0cbdf9c25f1f
HWChia <66512878+xmas21@users.noreply.github.com>
2020-07-05 14:34:29 +0800

    123

--- main.js
+++ main.js
@@ -30,3 +30,32 @@ console.log(e);
 var f = 5;
 f= f**3;
 console.log(f);
+
+// 指定運算子 適用於+ - * / % **
+var numl= 10;
+// numl=numl+5;
+numl**=5;
+console.log("指定運算子:"+numl);
+
+//自訂函式(方法) function、method
+//語法:
+//函式 函式名稱(參數1、參數2){程式內容}
+function functionA(){
+    console.log("我是函式 A");
+}
+
+// 需要呼叫才能執行
+functionA();
+
+function addTen(number){
+    number+= 10;
+    console.log("加十:"+number);
+}
+
+addTen(7);
+addTen(100);
+
+function mul(a,b){
+    console.log("數字相乘的結果:"+(a*b));
+}
+mul(99,7);
