f4462b93b754dfae0a62a0b12e21e1fb812b35f0
HWChia <66512878+xmas21@users.noreply.github.com>
2020-07-05 12:59:06 +0800

    123

--- main.js
+++ main.js
@@ -3,3 +3,30 @@ console.log("hello")
 // DOM 文件物件模式
 // 文件.透過ID搜尋元素("元素名稱").插入HTML="文字訊息";
 document.getElementById("box1").innerHTML="哈囉";
+
+
+// 變數 variable
+// 用於儲存資料
+// 資料類型
+var a= 10;     //整數
+var b= 1.5;    //浮點數
+var c= "文字"  //字串
+var d= true   //布林值 true 是、false 否
+
+
+// 運算子
+// 數學運算:+ - * / %(加減乘除餘)
+// 程式的=指定符號
+// =右邊先執行再指定給左邊
+a =a/3;
+console.log(a);
+
+// ++遞增1  --遞減1
+var e = 7;
+e++;
+console.log(e);
+
+// ES6 指定符號
+var f = 5;
+f= f**3;
+console.log(f);
